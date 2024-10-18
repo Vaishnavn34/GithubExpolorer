7f12de3ab473a700e283370d320d25e7599d0547
vaishnav60 <vaishnavnair699@gmail.com>
2024-10-18 10:43:28 +0530

    changwa

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,93 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>GitHub Repository Explorer</h1>
+    <input
+      v-model="username"
+      type="text"
+      placeholder="Enter GitHub username"
+    />
+    <button @click="fetchRepos">Get Repos</button>
+
+    <div v-if="loading">Loading...</div>
+    <div v-if="error" style="color: red;">{{ error }}</div>
+
+    <ul>
+      <li
+        v-for="repo in repos"
+        :key="repo.id"
+        @click="selectRepo(repo)"
+      >
+        {{ repo.name }} - ⭐ {{ repo.stargazers_count }}
+      </li>
+    </ul>
+
+    <div v-if="selectedRepo">
+      <h2>Description:</h2>
+      <p>{{ selectedRepo.description || 'No description available' }}</p>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      username: '',
+      repos: [],
+      selectedRepo: null,
+      loading: false,
+      error: '',
+    };
+  },
+  methods: {
+    async fetchRepos() {
+      this.loading = true;
+      this.error = '';
+      this.selectedRepo = null;
+      try {
+        const response = await fetch(`https://api.github.com/users/${this.username}/repos`);
+        if (!response.ok) {
+          throw new Error('User not found');
+        }
+        const data = await response.json();
+        this.repos = data;
+      } catch (err) {
+        this.error = err.message;
+      } finally {
+        this.loading = false;
+      }
+    },
+    selectRepo(repo) {
+      this.selectedRepo = repo;
+    },
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 20px;
+}
+
+input {
+  padding: 10px;
+  margin-right: 10px;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  cursor: pointer;
+  padding: 10px;
+  border: 1px solid #ccc;
+  margin: 5px 0;
+}
+
+li:hover {
+  background-color: #f0f0f0;
 }
 </style>
